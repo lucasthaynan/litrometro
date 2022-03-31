e9b5f9fab1e530d7eb9a59487228904b072bcafe
Lucas Thaynan <lucasthaynan.mcz@gmail.com>
2022-03-31 13:01:10 -0300

    ajustes nos butoes e carregamento de dados de todos os combustiveis

--- main.js
+++ main.js
@@ -14,35 +14,150 @@ fetch('https://raw.githubusercontent.com/lucasthaynan/litrometro/main/api/api-li
   .then(response => response.json() )
   .then(data => {
 
-    // verificando alguns dados
-    console.log(data[0].gasolina.mediana_ultimo_valor);
-    console.table(data[0].gasolina);
-
-    // verificando alguns dados
-    console.log(data[1].diesel.mediana_ultimo_valor);
-    console.table(data[1].diesel);
+    let valorIndexApi = 0;
+    let nomeTipoApi = "gasolina";
 
     // data de atualizacao
     let dataAtualizacao = (data[0].gasolina.data_atualizacao);
     document.querySelector('#atualizacao').innerHTML = dataAtualizacao;
 
     // pegando preco medio
-    let precoMedio = (data[0].gasolina.mediana_ultimo_valor);
+    let precoMedio = (data[valorIndexApi][nomeTipoApi].mediana_ultimo_valor);
+    document.querySelector('section.preco-menor').classList.add('dados-carregados')
     document.querySelector('.preco-medio .texto3').innerHTML = precoMedio;
-    let precoMedioPerc = (data[0].gasolina.mediana_variacao);
+    let precoMedioPerc = (data[valorIndexApi][nomeTipoApi].mediana_variacao);
     document.querySelector('.preco-medio .texto4').innerHTML = precoMedioPerc;
 
     // pegando preco menor
-    let precoMenor = (data[0].gasolina.menor_ultimo_valor);
+    let precoMenor = (data[valorIndexApi][nomeTipoApi].menor_ultimo_valor);
     document.querySelector('.preco-menor .texto3').innerHTML = precoMenor;
-    let precoMenorPerc = (data[0].gasolina.menor_valor_variacao);
+    let precoMenorPerc = (data[valorIndexApi][nomeTipoApi].menor_valor_variacao);
     document.querySelector('.preco-menor .texto4').innerHTML = precoMenorPerc;
 
     // pegando preco maior
-    let precoMaior = (data[0].gasolina.maior_ultimo_valor);
+    let precoMaior = (data[valorIndexApi][nomeTipoApi].maior_ultimo_valor);
     document.querySelector('.preco-maior .texto3').innerHTML = precoMaior;
-    let precoMaiorPerc = (data[0].gasolina.maior_valor_variacao);
-    document.querySelector('.preco-maior .texto4').innerHTML = precoMaiorPerc;   
+    let precoMaiorPerc = (data[valorIndexApi][nomeTipoApi].maior_valor_variacao);    
+    document.querySelector('.preco-maior .texto4').innerHTML = precoMaiorPerc;  
+
+    // data de atualizacao
+    // let dataAtualizacao = (data[0].gasolina.data_atualizacao);
+    // document.querySelector('#atualizacao').innerHTML = dataAtualizacao;
+
+    const btns = document.querySelectorAll('.container-buttons > button')
+
+    btns.forEach(button => {button.addEventListener('click', aciona)})
+    function aciona(e) {
+      
+      e.target.classList.add('ativo');
+      
+
+      
+
+      console.log(e.target.id);
+      let tipoCombustivel = e.target.id;
+      console.log(tipoCombustivel);
+
+      // document.querySelector('.conteudo-header button > #${tipoCombustivel}').classList.add('.ativo');
+
+      if (tipoCombustivel == "gasolina-comum") {
+        valorIndexApi = 0, 
+        nomeTipoApi = "gasolina";
+
+      } else if (tipoCombustivel == "gasolina-adt") {
+        valorIndexApi = 3, 
+        nomeTipoApi = "gasolina aditivada";
+
+      } else if (tipoCombustivel == "etanol") {
+        valorIndexApi = 2, 
+        nomeTipoApi = "etanol";
+
+      } else if (tipoCombustivel == "diesel") {
+        valorIndexApi = 1, 
+        nomeTipoApi = "diesel";
+
+      } else if (tipoCombustivel == "gnv") {
+        valorIndexApi = 4, 
+        nomeTipoApi = "gnv";
+      }; 
+      // else {
+      //   valorIndex = 4, 
+      //   nomeTipoApi = "gnv";
+      // };
+    
+    
+
+    console.log(tipoCombustivel);
+    console.log(valorIndexApi);
+    console.log(nomeTipoApi);
+
+    console.log(data[valorIndexApi][nomeTipoApi].mediana_ultimo_valor);     
+
+    
+
+    // pegando preco medio
+    let precoMedio = (data[valorIndexApi][nomeTipoApi].mediana_ultimo_valor);
+    document.querySelector('section.preco-menor').classList.add('dados-carregados')
+    document.querySelector('.preco-medio .texto3').innerHTML = precoMedio;
+    let precoMedioPerc = (data[valorIndexApi][nomeTipoApi].mediana_variacao);
+    document.querySelector('.preco-medio .texto4').innerHTML = precoMedioPerc;
+
+    // pegando preco menor
+    let precoMenor = (data[valorIndexApi][nomeTipoApi].menor_ultimo_valor);
+    document.querySelector('.preco-menor .texto3').innerHTML = precoMenor;
+    let precoMenorPerc = (data[valorIndexApi][nomeTipoApi].menor_valor_variacao);
+    document.querySelector('.preco-menor .texto4').innerHTML = precoMenorPerc;
+
+    // pegando preco maior
+    let precoMaior = (data[valorIndexApi][nomeTipoApi].maior_ultimo_valor);
+    document.querySelector('.preco-maior .texto3').innerHTML = precoMaior;
+    let precoMaiorPerc = (data[valorIndexApi][nomeTipoApi].maior_valor_variacao);    
+    document.querySelector('.preco-maior .texto4').innerHTML = precoMaiorPerc;  
+
+    
+
+     
+      
+    //   if (semaforo == "verde") {
+    //     mensagem = "Pode passar";
+    // } else if (semaforo == "vermelho") {
+    //     mensagem = "Pare";
+    // } else {
+    //     mensagem = "Atenção";
+    // }
+    
+    }
+
+    // // verificando alguns dados
+    // console.log(data[0][tipoCombustivel].mediana_ultimo_valor);
+    // console.table(data[0].gasolina);
+
+    // // verificando alguns dados
+    // console.log(data[1].diesel.mediana_ultimo_valor);
+    // console.table(data[1].diesel);
+
+    // // data de atualizacao
+    // let dataAtualizacao = (data[0].gasolina.data_atualizacao);
+    // document.querySelector('#atualizacao').innerHTML = dataAtualizacao;
+
+    // // pegando preco medio
+    // let precoMedio = (data[0].gasolina.mediana_ultimo_valor);
+    // document.querySelector('section.preco-menor').classList.add('dados-carregados')
+    // document.querySelector('.preco-medio .texto3').innerHTML = precoMedio;
+    // let precoMedioPerc = (data[0].gasolina.mediana_variacao);
+    // document.querySelector('.preco-medio .texto4').innerHTML = precoMedioPerc;
+
+    // // pegando preco menor
+    // let precoMenor = (data[0].gasolina.menor_ultimo_valor);
+    // document.querySelector('.preco-menor .texto3').innerHTML = precoMenor;
+    // let precoMenorPerc = (data[0].gasolina.menor_valor_variacao);
+    // document.querySelector('.preco-menor .texto4').innerHTML = precoMenorPerc;
+
+    // // pegando preco maior
+    // let precoMaior = (data[0].gasolina.maior_ultimo_valor);
+    // document.querySelector('.preco-maior .texto3').innerHTML = precoMaior;
+    // let precoMaiorPerc = (data[0].gasolina.maior_valor_variacao);    
+    // document.querySelector('.preco-maior .texto4').innerHTML = precoMaiorPerc;   
 
 
   })
