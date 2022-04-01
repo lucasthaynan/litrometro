4dd890f237c52125058dc4c15094cea0d1f9d7d2
Lucas Thaynan <lucasthaynan.mcz@gmail.com>
2022-04-01 16:13:28 -0300

    add api wordpress posts agencia tatu

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
+
+
+function mudarCorVariacao(status, element, elementCirculo) {
+  if (status == 'alta') {
+    element.style.color = "#E86C72",
+    elementCirculo.src = '/items/alta.svg';
+  } 
+  if (status == 'queda') {
+    element.style.color = "#61A449";
+    elementCirculo.src = '/items/queda.svg';
+  } 
+  if (status == 'sem-variacao') {
+    element.style.color = "#6CAFDF";
+    elementCirculo.src = '/items/sem-variacao.svg';
+  };
+}
+
 // REQUISICAO API
-
-// fetch('https://raw.githubusercontent.com/lucasthaynan/litrometroAPIv2/main/api/api-litrometro.json?token=GHSAT0AAAAAABMIDDLFFXUSQ3SFPAEZZZ46YSEVBBA')
-//   .then(response => response.json())
-//   .then(data => {
-
-//     console.log('olá');
-
-//     console.log(data);
-    
-//     })
 
 fetch('https://raw.githubusercontent.com/lucasthaynan/litrometro/main/api/api-litrometro.json')
   .then(response => response.json() )
@@ -26,11 +33,20 @@ fetch('https://raw.githubusercontent.com/lucasthaynan/litrometro/main/api/api-li
 
     // pegando preco medio
     let precoMedio = (data[valorIndexApi][nomeTipoApi].mediana_ultimo_valor);
+    
     document.querySelector('section.preco-medio').classList.add('dados-carregados')
     document.querySelector('.preco-medio .texto3').innerHTML = precoMedio;
     let precoMedioPerc = (data[valorIndexApi][nomeTipoApi].texto_variacao_mediana);
-    document.querySelector('.preco-medio .texto4').innerHTML = precoMedioPerc;
+    document.querySelector('.preco-medio .texto4').innerHTML = precoMedioPerc;    
 
+    // CHAMANDO A FUNÇÃO PARA MUDAR COR DO TEXTO DE VARIAÇÃO
+    let statusPrecoMedio = (data[valorIndexApi][nomeTipoApi].status_mediana);
+    let elementMedioCirculo = document.querySelector(".preco-medio .circulo"); 
+    console.log(statusPrecoMedio);
+    let elementMedio = document.querySelector(".preco-medio .bloco-percentual");
+    mudarCorVariacao(statusPrecoMedio, elementMedio, elementMedioCirculo);
+
+    
     // pegando preco menor
     let precoMenor = (data[valorIndexApi][nomeTipoApi].menor_ultimo_valor);
     document.querySelector('.preco-menor .texto3').innerHTML = precoMenor;
@@ -38,12 +54,26 @@ fetch('https://raw.githubusercontent.com/lucasthaynan/litrometro/main/api/api-li
     let precoMenorPerc = (data[valorIndexApi][nomeTipoApi].texto_menor_valor_variacao);
     document.querySelector('.preco-menor .texto4').innerHTML = precoMenorPerc;
 
+    // CHAMANDO A FUNÇÃO PARA MUDAR COR DO TEXTO DE VARIAÇÃO
+    let statusPrecoMenor = (data[valorIndexApi][nomeTipoApi].status_menor);   
+    let elementMenorCirculo = document.querySelector(".preco-menor .circulo"); 
+    let elementMenor = document.querySelector(".preco-menor .bloco-percentual");
+    mudarCorVariacao(statusPrecoMenor, elementMenor, elementMenorCirculo);
+
     // pegando preco maior
     let precoMaior = (data[valorIndexApi][nomeTipoApi].maior_ultimo_valor);
     document.querySelector('.preco-maior .texto3').innerHTML = precoMaior;
     document.querySelector('section.preco-maior').classList.add('dados-carregados')
     let precoMaiorPerc = (data[valorIndexApi][nomeTipoApi].texto_variacao_maior_valor);    
     document.querySelector('.preco-maior .texto4').innerHTML = precoMaiorPerc;  
+
+    // CHAMANDO A FUNÇÃO PARA MUDAR COR DO TEXTO DE VARIAÇÃO
+    let statusPrecoMaior = (data[valorIndexApi][nomeTipoApi].status_maior);   
+    let elementMaiorCirculo = document.querySelector(".preco-maior .circulo"); 
+    let elementMaior = document.querySelector(".preco-maior .bloco-percentual");
+    mudarCorVariacao(statusPrecoMaior, elementMaior, elementMaiorCirculo);
+
+    
 
     // data de atualizacao
     // let dataAtualizacao = (data[0].gasolina.data_atualizacao);
@@ -56,8 +86,8 @@ fetch('https://raw.githubusercontent.com/lucasthaynan/litrometro/main/api/api-li
       
       // e.target.classList.add('ativo');
       
-
-      
+      // let buttonColor = document.querySelector(".container-buttons button")
+      // e.target.style.backgroundColor = "#ffff"
 
       console.log(e.target.id);
       let tipoCombustivel = e.target.id;
@@ -102,11 +132,20 @@ fetch('https://raw.githubusercontent.com/lucasthaynan/litrometro/main/api/api-li
 
     // pegando preco medio
     let precoMedio = (data[valorIndexApi][nomeTipoApi].mediana_ultimo_valor);
+    
     document.querySelector('section.preco-medio').classList.add('dados-carregados')
     document.querySelector('.preco-medio .texto3').innerHTML = precoMedio;
     let precoMedioPerc = (data[valorIndexApi][nomeTipoApi].texto_variacao_mediana);
-    document.querySelector('.preco-medio .texto4').innerHTML = precoMedioPerc;
+    document.querySelector('.preco-medio .texto4').innerHTML = precoMedioPerc;    
 
+    // CHAMANDO A FUNÇÃO PARA MUDAR COR DO TEXTO DE VARIAÇÃO
+    let statusPrecoMedio = (data[valorIndexApi][nomeTipoApi].status_mediana);
+    let elementMedioCirculo = document.querySelector(".preco-medio .circulo"); 
+    console.log(statusPrecoMedio);
+    let elementMedio = document.querySelector(".preco-medio .bloco-percentual");
+    mudarCorVariacao(statusPrecoMedio, elementMedio, elementMedioCirculo);
+
+    
     // pegando preco menor
     let precoMenor = (data[valorIndexApi][nomeTipoApi].menor_ultimo_valor);
     document.querySelector('.preco-menor .texto3').innerHTML = precoMenor;
@@ -114,58 +153,28 @@ fetch('https://raw.githubusercontent.com/lucasthaynan/litrometro/main/api/api-li
     let precoMenorPerc = (data[valorIndexApi][nomeTipoApi].texto_menor_valor_variacao);
     document.querySelector('.preco-menor .texto4').innerHTML = precoMenorPerc;
 
+    // CHAMANDO A FUNÇÃO PARA MUDAR COR DO TEXTO DE VARIAÇÃO
+    let statusPrecoMenor = (data[valorIndexApi][nomeTipoApi].status_menor);   
+    let elementMenorCirculo = document.querySelector(".preco-menor .circulo"); 
+    let elementMenor = document.querySelector(".preco-menor .bloco-percentual");
+    mudarCorVariacao(statusPrecoMenor, elementMenor, elementMenorCirculo);
+
     // pegando preco maior
     let precoMaior = (data[valorIndexApi][nomeTipoApi].maior_ultimo_valor);
     document.querySelector('.preco-maior .texto3').innerHTML = precoMaior;
     document.querySelector('section.preco-maior').classList.add('dados-carregados')
     let precoMaiorPerc = (data[valorIndexApi][nomeTipoApi].texto_variacao_maior_valor);    
-    document.querySelector('.preco-maior .texto4').innerHTML = precoMaiorPerc;   
+    document.querySelector('.preco-maior .texto4').innerHTML = precoMaiorPerc;  
 
-    
+    // CHAMANDO A FUNÇÃO PARA MUDAR COR DO TEXTO DE VARIAÇÃO
+    let statusPrecoMaior = (data[valorIndexApi][nomeTipoApi].status_maior);   
+    let elementMaiorCirculo = document.querySelector(".preco-maior .circulo"); 
+    let elementMaior = document.querySelector(".preco-maior .bloco-percentual");
+    mudarCorVariacao(statusPrecoMaior, elementMaior, elementMaiorCirculo);   
 
-     
-      
-    //   if (semaforo == "verde") {
-    //     mensagem = "Pode passar";
-    // } else if (semaforo == "vermelho") {
-    //     mensagem = "Pare";
-    // } else {
-    //     mensagem = "Atenção";
-    // }
     
     }
-
-    // // verificando alguns dados
-    // console.log(data[0][tipoCombustivel].mediana_ultimo_valor);
-    // console.table(data[0].gasolina);
-
-    // // verificando alguns dados
-    // console.log(data[1].diesel.mediana_ultimo_valor);
-    // console.table(data[1].diesel);
-
-    // // data de atualizacao
-    // let dataAtualizacao = (data[0].gasolina.data_atualizacao);
-    // document.querySelector('#atualizacao').innerHTML = dataAtualizacao;
-
-    // // pegando preco medio
-    // let precoMedio = (data[0].gasolina.mediana_ultimo_valor);
-    // document.querySelector('section.preco-menor').classList.add('dados-carregados')
-    // document.querySelector('.preco-medio .texto3').innerHTML = precoMedio;
-    // let precoMedioPerc = (data[0].gasolina.mediana_variacao);
-    // document.querySelector('.preco-medio .texto4').innerHTML = precoMedioPerc;
-
-    // // pegando preco menor
-    // let precoMenor = (data[0].gasolina.menor_ultimo_valor);
-    // document.querySelector('.preco-menor .texto3').innerHTML = precoMenor;
-    // let precoMenorPerc = (data[0].gasolina.menor_valor_variacao);
-    // document.querySelector('.preco-menor .texto4').innerHTML = precoMenorPerc;
-
-    // // pegando preco maior
-    // let precoMaior = (data[0].gasolina.maior_ultimo_valor);
-    // document.querySelector('.preco-maior .texto3').innerHTML = precoMaior;
-    // let precoMaiorPerc = (data[0].gasolina.maior_valor_variacao);    
-    // document.querySelector('.preco-maior .texto4').innerHTML = precoMaiorPerc;   
-
+ 
 
   })
 
@@ -203,3 +212,43 @@ new Chart("Chart-js", {
   }
 });
 
+
+// REQUISAO POSTS AGENCIA TATURES
+
+let listaMaterias = [0,1,2,3];
+console.log(listaMaterias);
+
+
+
+function carregarMaterias(numeroContainerNews) {
+  fetch('https://agenciatatu.com.br/wp-json/wp/v2/posts?tags=69,70,64')
+    .then(response => response.json() )
+    .then(data => {
+      let tituloMateria = data[numeroContainerNews].title.rendered;
+      let urlMateria = data[numeroContainerNews].link; 
+      let apiFotosMateria = data[numeroContainerNews]._links['wp:featuredmedia'][0]['href'];
+      console.log(tituloMateria);
+      console.log(urlMateria);
+
+      fetch(apiFotosMateria)
+        .then(response => response.json() )
+        .then(data => {
+          let urlFotoMateria = data.source_url;
+          console.log(urlFotoMateria);   
+          
+          // ADICIONANDO URL DA MATERIA E FOTOS NA PAGINA        
+
+          document.querySelectorAll('section.noticias .news > img')[numeroContainerNews].src = urlFotoMateria;
+          console.log()
+          document.querySelectorAll('section.noticias .news > a')[numeroContainerNews].href = urlMateria;
+          document.querySelectorAll('section.noticias .news > a')[numeroContainerNews].innerHTML = tituloMateria;
+
+        })    
+    })
+}
+
+for (let materia in listaMaterias) {
+  carregarMaterias(materia)
+}
+
+  
