a2b4d3ebe9afc8baca966b9e26043a6d37965653
Lucas Thaynan <lucasthaynan.mcz@gmail.com>
2022-04-14 10:44:48 -0300

    testes de gráfico

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+// FUNÇÃO QUE MUDA DE COR O TRECHO DE VARIAÇÃO
 function mudarCorVariacao(status, element, elementCirculo) {
   if (status == 'alta') {
     element.style.color = "#E86C72",
@@ -15,39 +15,37 @@ function mudarCorVariacao(status, element, elementCirculo) {
   };
 }
 
-// REQUISICAO API
+
+graficoCombustivel('0', 'gasolina')
+
+
+// REQUISICAO API DOS DADOS DOS 3 PRIMEIROS BLOCOS
 
 fetch('https://raw.githubusercontent.com/lucasthaynan/litrometro/main/api/dados_do_dia_litrometro.json')
   .then(response => response.json() )
   .then(data => {
 
-    // console.log(data[0].gasolina)
-    // console.table(data[0].gasolina)
-
+    // valores iniciais das variáveis ao abrir a página
     let valorIndexApi = 0;
     let nomeTipoApi = "gasolina";
 
-    // data de atualizacao
+    // data de atualização
     let dataAtualizacao = (data[0].gasolina.data_atualizacao);
     document.querySelector('#atualizacao').innerHTML = dataAtualizacao;
 
+    // chamando a função de execução do botão 'gasolina-comum'
     aciona('gasolina-comum')
 
-
+    // pegando todos os butões de tipos de combustíveis e fazendo uma ação para cada um deles ao ser clicado
     const btns = document.querySelectorAll('.container-buttons > button')
 
     btns.forEach(button => {button.addEventListener('click', e => aciona(e.target.id))})
     function aciona(tipoCombustivel) {
       const btns = document.querySelectorAll('.container-buttons > button')
       btns.forEach(button => button.classList.remove('ativo'));
-      document.querySelector('#' + tipoCombustivel).classList.add('ativo');
+      document.querySelector('#' + tipoCombustivel).classList.add('ativo');    
 
-      
-
-            
-      
-      // console.log(tipoCombustivel);
-
+      // verificando se a classe do butão tem o mesmo nome das chaves no arquivo JSON (api)
       if (tipoCombustivel == "gasolina-comum") {
         valorIndexApi = 0, 
         nomeTipoApi = "gasolina";
@@ -70,19 +68,12 @@ fetch('https://raw.githubusercontent.com/lucasthaynan/litrometro/main/api/dados_
       };     
 
       // GERANDO GRÁFICO POR COMBUSTÍVEL
-      grafico_combustivel(valorIndexApi, nomeTipoApi)
     
 
-    // console.log(tipoCombustivel);
-    // console.log(valorIndexApi);
-    // console.log(nomeTipoApi);
+      graficoCombustivel(valorIndexApi, nomeTipoApi); 
 
-    // console.log(data[valorIndexApi][nomeTipoApi].mediana_ultimo_valor);     
-    
 
-    
-
-    // pegando preco medio
+    // PEGANDO E EXIBINDO PREÇO MÉDIO
     let precoMedio = (data[valorIndexApi][nomeTipoApi].mediana_ultimo_valor);
     
     document.querySelector('section.preco-medio').classList.add('dados-carregados')
@@ -90,41 +81,39 @@ fetch('https://raw.githubusercontent.com/lucasthaynan/litrometro/main/api/dados_
     let precoMedioPerc = (data[valorIndexApi][nomeTipoApi].texto_variacao_mediana);
     document.querySelector('.preco-medio .texto4').innerHTML = precoMedioPerc;    
 
-    // CHAMANDO A FUNÇÃO PARA MUDAR COR DO TEXTO DE VARIAÇÃO
+   // chamando a função de mudança de cor do texto de variação
     let statusPrecoMedio = (data[valorIndexApi][nomeTipoApi].status_mediana);
-    let elementMedioCirculo = document.querySelector(".preco-medio .circulo"); 
-    console.log(statusPrecoMedio);
+    let elementMedioCirculo = document.querySelector(".preco-medio .circulo");     
     let elementMedio = document.querySelector(".preco-medio .bloco-percentual");
     mudarCorVariacao(statusPrecoMedio, elementMedio, elementMedioCirculo);
 
+
     
-    // pegando preco menor
+    // PEGANDO E EXIBINDO PREÇO MENOR
     let precoMenor = (data[valorIndexApi][nomeTipoApi].menor_ultimo_valor);
     document.querySelector('.preco-menor .texto3').innerHTML = precoMenor;
     document.querySelector('section.preco-menor').classList.add('dados-carregados')
     let precoMenorPerc = (data[valorIndexApi][nomeTipoApi].texto_menor_valor_variacao);
     document.querySelector('.preco-menor .texto4').innerHTML = precoMenorPerc;
 
-    // CHAMANDO A FUNÇÃO PARA MUDAR COR DO TEXTO DE VARIAÇÃO
+    // chamando a função de mudança de cor do texto de variação
     let statusPrecoMenor = (data[valorIndexApi][nomeTipoApi].status_menor);   
     let elementMenorCirculo = document.querySelector(".preco-menor .circulo"); 
     let elementMenor = document.querySelector(".preco-menor .bloco-percentual");
     mudarCorVariacao(statusPrecoMenor, elementMenor, elementMenorCirculo);
 
-    // pegando preco maior
+    // PEGANDO E EXIBINDO PREÇO MAIOR
     let precoMaior = (data[valorIndexApi][nomeTipoApi].maior_ultimo_valor);
     document.querySelector('.preco-maior .texto3').innerHTML = precoMaior;
     document.querySelector('section.preco-maior').classList.add('dados-carregados')
     let precoMaiorPerc = (data[valorIndexApi][nomeTipoApi].texto_variacao_maior_valor);    
     document.querySelector('.preco-maior .texto4').innerHTML = precoMaiorPerc;  
 
-    // CHAMANDO A FUNÇÃO PARA MUDAR COR DO TEXTO DE VARIAÇÃO
+    // chamando a função de mudança de cor do texto de variação
     let statusPrecoMaior = (data[valorIndexApi][nomeTipoApi].status_maior);   
     let elementMaiorCirculo = document.querySelector(".preco-maior .circulo"); 
     let elementMaior = document.querySelector(".preco-maior .bloco-percentual");
-    mudarCorVariacao(statusPrecoMaior, elementMaior, elementMaiorCirculo);   
-
-    
+    mudarCorVariacao(statusPrecoMaior, elementMaior, elementMaiorCirculo);       
     
     } 
 
@@ -132,101 +121,158 @@ fetch('https://raw.githubusercontent.com/lucasthaynan/litrometro/main/api/dados_
 
 // GERANDO GRAFICO
 
-function grafico_combustivel(valorIndexApi, nomeTipoApi) {
+
+
+// function graficoCombustivelAtualizacao(valorIndexApi, nomeTipoApi) {
+//   fetch('https://raw.githubusercontent.com/lucasthaynan/litrometro/main/api/historico.json')
+//   .then(response => response.json())
+//   .then(data => {
+
+//     let variaveisGrafico = Object.keys(data[valorIndexApi][nomeTipoApi].preco_medio);
+
+//     let valoresGrafico = Object.values(data[valorIndexApi][nomeTipoApi].preco_medio); 
+
+//     // console.log(valoresGrafico);
+//     // console.log(variaveisGrafico);
+
+//     updateGrafico('grafico', variaveisGrafico, valoresGrafico);
+
+//     console.log('graficoCombustivelAtualizacao')
+    
+    
+//    } 
+//   )
+// }
+
+// function graficoHistotico(variaveisGrafico, valoresGrafico){  
+//   var grafico = new Chart("Chart-js", {
+//     type: "line",
+//     data: {
+//       // valores do eixo X
+//       labels: variaveisGrafico,
+//       datasets: [{
+//         // valores do eixo Y
+//         data: valoresGrafico,
+//         borderColor: '#E86C72',
+//         fill: true,
+//         label: 'Teste'
+//       }]
+
+//     },
+//     options: {
+//       legend: {display: false},
+
+//       interaction: {
+//           intersect: false,
+//           mode: 'index',
+//         },
+        
+//     }
+//   });
+  
+// }
+    
+
+
+function graficoCombustivel(valorIndexApi, nomeTipoApi) {
   fetch('https://raw.githubusercontent.com/lucasthaynan/litrometro/main/api/historico.json')
   .then(response => response.json())
   .then(data => {
 
-    // console.log(data[0].gasolina.preco_medio)
+    let variaveisGrafico = Object.keys(data[valorIndexApi][nomeTipoApi].preco_medio);
 
-    console.log(Object.keys(data[valorIndexApi][nomeTipoApi].preco_medio));
+    let valoresGrafico = Object.values(data[valorIndexApi][nomeTipoApi].preco_medio);
 
-    console.log(Object.values(data[valorIndexApi][nomeTipoApi].preco_medio));
+    console.log('graficoCombustivel')
 
-    // console.log(Object.values(data[0]).filter(d => d != 'Gasolina'))
+    // grafico.destroy();
+
+    console.log('graficoCombustivelTESTE')
 
     var grafico = new Chart("Chart-js", {
       type: "line",
       data: {
         // valores do eixo X
-        labels: Object.keys(data[valorIndexApi][nomeTipoApi].preco_medio),
+        labels: variaveisGrafico,
         datasets: [{
           // valores do eixo Y
-          data: Object.values(data[valorIndexApi][nomeTipoApi].preco_medio),
+          data: valoresGrafico,
           borderColor: '#E86C72',
           fill: true,
           label: nomeTipoApi
         }]
-
+    
       },
       options: {
         legend: {display: false},
-
+    
         interaction: {
             intersect: false,
             mode: 'index',
           },
           
       }
-    });
+    });    
+
+
+    
    } 
   )
-
 }
 
+// function resetGrafico() {
+//   grafico.destroy();
+//   grafico.
 
+// }
 
-// GRÁFICO
-
-// function updateChart(){
-//   grafico.data.datasets[0].data = [10,10,30]; grafico.update();
+// function updateGrafico(grafico, variaveisGrafico, valoresGrafico) {{
+//   grafico.data.labels = variaveisGrafico;
+//   grafico.data.datasets.data = valoresGrafico;
 //   };
+//   console.log('Atualização')
+//   chart.update();}
 
-// updateChart()
+
+
+
 
 
 // REQUISAO POSTS AGENCIA TATU
-
 
 function carregarMaterias(numeroContainerNews) {
   fetch('https://raw.githubusercontent.com/lucasthaynan/litrometro/main/api/materias_tatu.json')
     .then(response => response.json())
     .then(data => {
-      console.log('ok')
-      console.log(data)
+
+      // console.log('ok')
+      // console.log(data)
+
       let tituloMateria = data[numeroContainerNews].titulo_materia;
       let urlMateria = data[numeroContainerNews].url_materia; 
       let urlFotoMateria = data[numeroContainerNews].url_foto_materia;
-      //     // console.log(urlFotoMateria);
-      // let apiFotosMateria = data[numeroContainerNews]._links['wp:featuredmedia'][0]['href'];
-      console.log(tituloMateria);
-      console.log(urlMateria);
-      console.log(urlFotoMateria)
 
-      // fetch(apiFotosMateria)
-      //   .then(response => response.json() )
-      //   .then(data => {
-      //     let urlFotoMateria = data.source_url;
-      //     // console.log(urlFotoMateria);   
+      // console.log(tituloMateria);
+      // console.log(urlMateria);
+      // console.log(urlFotoMateria)
           
       // ADICIONANDO URL DA MATERIA E FOTOS NA PAGINA        
-
       document.querySelectorAll('section.noticias .news > img')[numeroContainerNews].src = urlFotoMateria;
-      // console.log()
+
       document.querySelectorAll('section.noticias .news > a')[numeroContainerNews].href = urlMateria;
+
       document.querySelectorAll('section.noticias .news > a')[numeroContainerNews].innerHTML = tituloMateria;
 
-      //   })    
     })
 };
 
 // CHAMANDO A FUNÇÃO DE CARREGAR AS MATERIAS
-// let listaMaterias = [0,1,2,3];
 
-// for (let materia in listaMaterias) {
-//   carregarMaterias(materia)
-// };
 carregarMaterias(0);
 carregarMaterias(1);
 carregarMaterias(2);
 carregarMaterias(3);
+
+// grafico_combustivel(2, 'etanol')
+
+// grafico_combustivel(valorIndexApi, nomeTipoApi)
